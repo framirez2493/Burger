cefb4633648124af887b06efdcb9ad0cc70cf1d2
fernando_R <fer0624@gmail.com>
2018-09-29 14:33:20 -0700

    4 attempt

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection;
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,    
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+});
 
-if(process.env.JAWSDB_URL) {
+
+/*if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
@@ -14,7 +21,7 @@ else {
         password: "root",
         database: "burgers_db"
     });
-}
+}*/
 
 // Make connection.
 connection.connect(function(err) {
